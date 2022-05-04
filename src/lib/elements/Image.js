2835092928c7fd50c5869fe4f94fc3727dd09f3d
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2022-05-04 17:29:49 +0200

    image: add placeholder loader
    * add support to render Image as Item.Image
    * introduce new prop 'loadFallbackFirst' to prioritize
      fallback image load first. Use when loading the src
      is a potentially slow operation

--- src/lib/elements/Image.js
+++ src/lib/elements/Image.js
@@ -6,26 +6,64 @@
 
 import PropTypes from "prop-types";
 import React, { Component } from "react";
-import { Image as SUIImage } from "semantic-ui-react";
+import { Image as SUIImage, Item, Ref } from "semantic-ui-react";
+import axios from "axios";
 
 /**
  * Primary UI Image component providing a fallback url if src one is not
  * able to be resolved. This is a thin layer on top of the <img> element.
  */
 export class Image extends Component {
+  async componentDidMount() {
+    const { loadFallbackFirst, src } = this.props;
+    if (loadFallbackFirst) {
+      try {
+        await axios.get(src);
+        this.setSrc(this.myRef.current, src);
+      } catch (error) {
+        // Fallback image is already loaded
+      }
+    }
+  }
+  myRef = React.createRef();
+
+  setSrc = (currentTarget, src) => {
+    const { asItem } = this.props;
+    if (asItem) {
+      // Item.Image is wrapping the <img> in a div element
+      currentTarget.childNodes[0].src = src;
+    } else {
+      currentTarget.src = src;
+    }
+  };
+
   render() {
-    const { alt, src, fallbackSrc, className, ...UIprops } = this.props;
+    const { alt, className, src, fallbackSrc, loadFallbackFirst, asItem, ...UIprops } =
+      this.props;
+    const ImageCmp = asItem ? Item.Image : SUIImage;
+    const loadingClass = loadFallbackFirst ? `${className} placeholder` : className;
+    const url = loadFallbackFirst ? fallbackSrc : src;
     return (
-      <SUIImage
-        className={className}
-        alt={alt}
-        src={src}
-        onError={({ currentTarget }) => {
-          currentTarget.onerror = null; // prevents looping
-          currentTarget.src = fallbackSrc;
-        }}
-        {...UIprops}
-      />
+      <Ref innerRef={this.myRef}>
+        <ImageCmp
+          className={loadingClass}
+          alt={alt}
+          src={url}
+          {...(!loadFallbackFirst && {
+            onError: ({ currentTarget }) => {
+              currentTarget.onerror = null; // prevents looping
+              this.setSrc(currentTarget, fallbackSrc);
+            },
+            onLoad: () => {
+              // Control the loader via ref to make it immediately invisible
+              if (!loadFallbackFirst) {
+                this.myRef.current.classList.remove("placeholder");
+              }
+            },
+          })}
+          {...UIprops}
+        />
+      </Ref>
     );
   }
 }
@@ -35,10 +73,14 @@ Image.propTypes = {
   fallbackSrc: PropTypes.string,
   className: PropTypes.string,
   alt: PropTypes.string,
+  asItem: PropTypes.bool,
+  loadFallbackFirst: PropTypes.bool,
 };
 
 Image.defaultProps = {
   className: "",
   alt: "No image found",
   fallbackSrc: "/static/images/square-placeholder.png",
+  asItem: false,
+  loadFallbackFirst: false,
 };
